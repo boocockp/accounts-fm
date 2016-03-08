7f8bb4e14c4e6eca7333c7ec2ce9e1e930160f6c
paulb <paulb@ashridgetech.com>
2016-03-08 09:47:46 +0000

    Web components displaying tran entry and accounts table but not updating

--- main/view.js
+++ main/view.js
@@ -39,6 +39,45 @@ class AccountsTableView {
     }
 }
 
+var AccountsTableProto = Object.create(HTMLElement.prototype, {
+    accountSummaries: {
+        get: function () {
+            return this._accountSummaries;
+        },
+        set: function (as) {
+            this._accountSummaries = as;
+        }
+    }
+});
+
+AccountsTableProto.attachedCallback = function () {
+    let accToRow = a => {
+        return `<tr>
+                    <td>${a.details.value.name}</td>
+                    <td>${a.balance.value}</td>
+                    </tr>
+                    `
+    };
+
+    let accountRows = this._accountSummaries.map(accToRow).join('\n').value;
+    this.innerHTML = `<table>
+                        <thead>
+                        <tr>
+                        <th>Name</th>
+                        <th>Balance</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        ${accountRows}
+                        </tbody>
+                    </table>
+                    `;
+};
+
+var AccountsTable = document.registerElement('accounts-table', {prototype: AccountsTableProto});
+
+
+
 class AccountRowsView {
     constructor(accountSummaries) {
         let accToRow = a => {
@@ -77,14 +116,32 @@ let rowHtml = (acct) => {
         `;
 };
 
-class TransactionEntryView {
 
-    constructor(accountInfos) {
-        this._accountInfos = accountInfos
+
+var TransactionEntryProto = Object.create(HTMLElement.prototype, {
+    accountInfos: {
+        get: function () {
+            return this._accountInfos;
+        },
+        set: function (ai) {
+            this._accountInfos = ai;
+        }
+    },
+    foo: {
+        value: function () {
+            alert('foo() called');
+        }
     }
+});
 
-    get html() {
-        this._html = this._html || new ConstantCachedSequence(`
+TransactionEntryProto.attachedCallback = function () {
+    let accInfoToOption = a => {
+        let accInfo = a.value;
+        return `<option value="${accInfo.id}">${accInfo.name}</option>`
+    };
+
+    let accountOptions = this._accountInfos.map(accInfoToOption).join('\n').value;
+    this.innerHTML = `
         <h3>Transaction Entry</h3>
         <form action="">
             <div>
@@ -98,8 +155,9 @@ class TransactionEntryView {
 
             <div>
                 <label>Posting 1</label>
+                <account-select account-infos="{{accountInfos}}"></account-select>
                 <select name="posting1.accountId">
-                   {{accountOptions}}
+                   ${accountOptions}
                 </select>
                 <select name="posting1.type">
                     <option value="DR">Debit</option>
@@ -111,7 +169,7 @@ class TransactionEntryView {
             <div>
                 <label>Posting 2</label>
                 <select name="posting2.accountId">
-                    {{accountOptions}}
+                    ${accountOptions}
                 </select>
                 <select name="posting2.type">
                     <option value="DR">Debit</option>
@@ -125,23 +183,11 @@ class TransactionEntryView {
                 <button type="submit">Save</button>
             </div>
         </form>
-        `);
+        `;
+};
 
-        return this._html;
-    }
+var TransactionEntry = document.registerElement('transaction-entry', {prototype: TransactionEntryProto});
 
-    get eventsRequired() {
-        return ['change'];
-    }
-
-    get accountOptions() {
-        return this._accountOptions || (this._accountOptions = new AccountOptionsView(this._accountInfos));
-    }
-
-    notifyEvent(e) {
-
-    }
-}
 
 class AccountOptionsView {
     constructor(accountInfos) {
@@ -171,37 +217,54 @@ class AccountOptionsView {
 
 }
 
+var AccountSelectProto = Object.create(HTMLElement.prototype, {
+    name: {
+        get: function () {
+            return this._name;
+        },
+        set: function (n) {
+            this._name = n;
+        }
+    },
+    accountInfos: {
+        get: function () {
+            let elWithData = this.parentElement;
+            while (elWithData && !_.hasIn(elWithData, 'accountInfos')) {
+                elWithData = elWithData.parentElement;
+            }
 
-let show = function (viewFunction, dataSequence, container) {
-    dataSequence.onChange((data) => container.innerHTML = viewFunction(data));
+            return elWithData && elWithData.accountInfos;
+        }
+    }
+});
+
+AccountSelectProto.attachedCallback = function () {
+    console.log(this.tagName, 'attachedCallback');
+    let accInfoToOption = a => {
+        let accInfo = a.value;
+        return `<option value="${accInfo.id}">${accInfo.name}</option>`
+    };
+
+    let accountOptions = this.accountInfos ? this.accountInfos.map(accInfoToOption).join('\n').value : [];
+    this.innerHTML = `
+                <select name="${this.name}">
+                   ${accountOptions}
+                </select>
+        `;
 };
 
-//show(accountsTableView, generalLedger.accountsByName, document.getElementById('table'));
-//show(transactionEntryView, generalLedger.accountInfos, document.getElementById('transactions'));
+AccountSelectProto.attributeChangedCallback = function(attrName, oldVal, newVal) {
+    console.log(this.tagName, 'attributeChangedCallback', attrName, oldVal, newVal);
+};
+
+var AccountSelect = document.registerElement('account-select', {prototype: AccountSelectProto});
 
 
-function render(viewModel, container) {
-    viewModel.html.onChange(function (html) {
-        container.innerHTML = html;
 
-        let placeholderSnapshot = document.evaluate('.//text()[contains(., "{{") and contains(., "}}")]', container, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
-        // console.log('xpath placeholders', placeholderSnapshot);
+var transactionEntryView = new TransactionEntry();
+transactionEntryView.accountInfos = generalLedger.accountInfos;
+document.getElementById('transactions').appendChild(transactionEntryView);
 
-        let placeholderNodes = [];
-        for (let i = 0; i < placeholderSnapshot.snapshotLength; i++) {
-            placeholderNodes.push(placeholderSnapshot.snapshotItem(i));
-        }
-
-        placeholderNodes.forEach( n => {
-            let placeholderParent = n.parentNode;
-            let placeholderName = n.textContent.match(/\{\{ *(\w+) *\}\}/)[1];
-            let placeholderViewModel = viewModel[placeholderName];
-            render(placeholderViewModel, placeholderParent);
-        });
-    })
-}
-
-let transactionEntryView = new TransactionEntryView(generalLedger.accountInfos);
- let accountsTableView = new AccountsTableView(generalLedger.accountSummaries);
-render(transactionEntryView, document.getElementById('transactions'));
-render(accountsTableView, document.getElementById('table'));
+let accountsTableView = new AccountsTable();
+accountsTableView.accountSummaries = generalLedger.accountSummaries
+document.getElementById('table').appendChild(accountsTableView);
